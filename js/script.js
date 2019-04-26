70f245d7bb7019f563a11f53976897abc9b21e48
Dylan <dylanfairbanks@icloud.com>
2019-04-25 18:36:39 -0600

    added typed.js to give a typed effect to the sub title.  also added the katakana for Fairbanks

--- js/script.js
+++ js/script.js
@@ -3,5 +3,15 @@ $(document).ready(function()
   $('#slides').superslides({
     animation: 'fade',
     play: 0
+
+
+  })
+
+  var typed = new Typed(".typed" ,{
+    strings: ["software developer."],
+    typeSpeed: 70,
+    startDelay: 800,
+    loop: false,
+    showCursor: false
   })
 });
