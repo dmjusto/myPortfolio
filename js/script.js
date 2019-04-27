197f5d460eccdd5ce6a9762e7c749bd1454f1a42
Dylan <dylanfairbanks@icloud.com>
2019-04-26 20:36:08 -0600

    installed owl carousel for the skills section

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
-$(document).ready(function()
-{
+$(document).ready(function(){
+
   $('#slides').superslides({
     animation: 'fade',
-    play: 0
-
-
-  })
+    play: 0,
+    pagination: false
+  });
 
   var typed = new Typed(".typed" ,{
     strings: ["Software Developer."],
@@ -13,5 +12,23 @@ $(document).ready(function()
     startDelay: 800,
     loop: false,
     showCursor: false
-  })
+  });
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+  });
+
 });
